437b697f13fe49b03108173d9b351996d3182518
Filip Palusiński <palusinskifilip@gmail.com>
2022-06-03 15:57:10 +0200

    Add StockRepository

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -1,5 +1,8 @@
 package com.plcoding.stockmarketapp.data.remote
 
+
+import com.plcoding.stockmarketapp.BuildConfig
+import com.plcoding.stockmarketapp.data.remote.StockApi.Companion.API_KEY
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,11 +12,12 @@ interface StockApi {
 
     @GET("query?function=LISTING_STATUS")
     suspend fun getListings(
-        @Query("apikey") apiKey: String
+        @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
 
     companion object {
-        const val API_KEY = "WI231GMUXYUBG9KY"
+        //APIkey is free and just for learning purpose I made it visible
+        private const val API_KEY = "WI231GMUXYUBG9KY"
         const val BASE_URL = "https://alphavantage.co"
     }
 }

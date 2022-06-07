237a1e27a15e8ab5c82dd0f5ffe4feec0fc0c3d0
Filip Palusiński <palusinskifilip@gmail.com>
2022-06-07 23:24:34 +0200

    Add CompanyInfo Data

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -3,6 +3,7 @@ package com.plcoding.stockmarketapp.data.remote
 
 import com.plcoding.stockmarketapp.BuildConfig
 import com.plcoding.stockmarketapp.data.remote.StockApi.Companion.API_KEY
+import com.plcoding.stockmarketapp.data.remote.dto.CompanyInfoDto
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,6 +15,18 @@ interface StockApi {
     suspend fun getListings(
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
+
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): ResponseBody
+
+    @GET("query?function=OVERVIEW")
+    suspend fun getCompanyInfo(
+        @Query("syombol") symbol: String,
+        @Query("apikey") apiKey: String = API_KEY
+    ): CompanyInfoDto
 
     companion object {
         //APIkey is free and just for learning purpose I made it visible
